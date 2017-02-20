5671b381d869f12bbe7e6def980b330cd0630d64
heyanqiu <804350300@qq.com>
2017-02-20 16:07:37 +0800

    first time

---
+++ app/src/main/java/com/qf/lingering/utils/HttpUtil.java
@@ -0,0 +1,10 @@
+package com.qf.lingering.utils;
+
+/**
+ * Created by heyanqiu on 2017/2/20.
+ * 网络下载类
+ */
+
+public class HttpUtil {
+
+}
---
+++ app/src/main/java/com/qf/lingering/utils/Util.java
@@ -0,0 +1,9 @@
+package com.qf.lingering.utils;
+
+/**
+ * Created by heyanqiu on 2017/2/20.
+ * 默认的常量和系统的常量
+ */
+
+public class Util {
+}
